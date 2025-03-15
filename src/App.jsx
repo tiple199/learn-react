a13a100d6ac8922e62ffbdfc970479c130fb10a4
Tiep <lenhotiep1992004@gmail.com>
2025-03-15 15:02:34 +0700

    vd 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
+const MyComponent = () => {
+  return (
+    <div>Lê Nho Tiệp</div>
+  );
+}
 
 const App = () => {
   const [count, setCount] = useState(0)
 
-  const myFunction = () =>{
-    console.log("Hellooo");
-  }
-  myFunction()
 
   return (
     <>
@@ -24,6 +24,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello world</h1>
+      <MyComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
