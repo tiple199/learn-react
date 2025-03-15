88031d6394575ab387ed1a31bab8989f5011d57e
Tiep <lenhotiep1992004@gmail.com>
2025-03-15 16:01:46 +0700

    vd 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,12 @@
+import "./style.css"
 const MyComponent = () => {
   return (
-    <div>Lê Nho Tiệp</div>
+    <>
+        <div className="child" style={
+            {borderRadius: "10px"}
+        }>Lê Nho Tiệp</div>
+        <div>Hehe</div>
+    </>
   );
 }
 
