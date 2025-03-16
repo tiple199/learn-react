7f90b3170816296976a973c2ed9f0d0a03aadbab
Tiep <lenhotiep1992004@gmail.com>
2025-03-16 20:30:35 +0700

    vd 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,14 @@
 import "./style.css"
 const MyComponent = () => {
+  // const name = "Tiệp";
+  const name = [1,2,3];
   return (
     <>
+      {/* trong phần style {} thứ nhất để viết code javascript trong html còn cái thứ hai để thể hiện đó là một object vì thế ví dụ như borderRadius là dạng thế kia */}
         <div className="child" style={
             {borderRadius: "10px"}
-        }>Lê Nho Tiệp</div>
+        }>Lê Nho {JSON.stringify(name)}</div>
+        <div>{console.log("Hello")}</div>
         <div>Hehe</div>
     </>
   );
