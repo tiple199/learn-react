83e56bea12a62a8e5280720353af09e36e55af68
Tiep <lenhotiep1992004@gmail.com>
2025-03-17 09:45:49 +0700

    vd 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,12 +2,24 @@ const TodoNew = (props) =>{
   console.log(">>> check point",props)
   const {addNewTodo} =  props;
   // addNewTodo("Tiep");
-    return(
-        <div className="todo-new">
-        <input className="input" type="text" />
-        <button className="btn">Add</button>
-      </div>
-    )
+
+  const handleClick = () => {
+    alert("Click me")
+  }
+  const handleOnChange = (name) => {
+    console.log(">>> handleOnChang", name)
+  }
+  return(
+      <div className="todo-new">
+      <input className="input"
+       type="text" 
+       onChange={(e) => handleOnChange(e.target.value)}
+       />
+      <button style={{cursor: "pointer"}} className="btn"
+      onClick={handleClick}
+      >Add</button>
+    </div>
+  )
 }
 
 export default TodoNew;
