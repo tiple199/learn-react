037215e5e4cbf813fb7e299e769ed26d04e8155f
Tiep <lenhotiep1992004@gmail.com>
2025-03-19 16:20:23 +0700

    vd 47

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from "./components/todo/TodoData";
 import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 import { useState } from "react";
+import Header from "./layout/header";
+import Footer from "./layout/footer";
 const App = () => {
   const [todoList,setTodoList] = useState([
     // {id: 1,name:"Learning React"},
@@ -30,6 +32,8 @@ const App = () => {
   
 
   return (
+    <>
+    <Header/>
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew 
@@ -46,6 +50,8 @@ const App = () => {
       </div>
       }
     </div>
+    <Footer/>
+    </>
   )
 }
 
