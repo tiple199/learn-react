54c7f2f474fb3546174bda5a26e55d34220137d9
Tiep <lenhotiep1992004@gmail.com>
2025-04-12 10:49:08 +0700

    vd 89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,63 @@
+import { Button, Form, Input } from "antd";
+
 const RegisterPage = () =>{
+    const [form] = Form.useForm();
+    const onFinish = (value)=>{
+        console.log(value);
+        
+    }
     return(
-        <div>Register page</div>
+        <Form
+        form={form}
+        layout="vertical"
+    
+        onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        >
+    
+        <div style={{
+            // display: "flex",
+            // flexDirection:"column",
+            margin:"50px"}}>
+            <Form.Item
+            label="Full Name"
+            name="fullName"
+    //       rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+            <Input />
+            </Form.Item>
+            <Form.Item
+            label="Email"
+            name="email"
+    //       rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+            <Input />
+            </Form.Item>
+            <Form.Item
+            label="Password"
+            name="password"
+    //       rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+            <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+            label="Phone Number"
+            name="phone"
+    //       rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+            <Input />
+            </Form.Item>
+
+
+
+            <div>
+                <Button onClick={()=>form.submit()} type="primary">Register</Button>
+            </div>
+        </div>
+    
+    </Form>
     )
 }
 export default RegisterPage;
